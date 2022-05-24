43ae2a7fcda51911a3598294b70bcc30d9fe3704
Mélanie Sarrouy <sarsarmel@gmail.com>
2022-05-24 17:18:18 +0200

    responsive page contact 0k + clean assets

--- src/React/components/3pagesPortfolio/GraphismePortfolio.jsx
+++ src/React/components/3pagesPortfolio/GraphismePortfolio.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import H3TrioTitle from '../../layout/H3TrioTitle'
+import { SectionPortfolio } from '../../../styles/components/3pagesPortfolio/webPortfolio'
+import wait from '../../../assets/illustrations/Waiting_Monochromatic.svg'
+import { DivImgWait, PTextProvisoire } from '../../../styles/components/3pagesPortfolio/graphismePortfolio'
 
-const title='Portfolio Graphisme en cours de construction...'
+const title = 'Portfolio Graphisme en cours de construction...'
+
+
 const GraphismePortfolio = () => {
   return (
-    <div>
-      <H3TrioTitle title={title}/>
-    </div>
+    <SectionPortfolio id="gallery">
+      <PTextProvisoire>{title}</PTextProvisoire>
+      <DivImgWait>
+        <img src={wait} alt="illustration attente" width={350} />
+      </DivImgWait>
+    </SectionPortfolio>
   )
 }
 
